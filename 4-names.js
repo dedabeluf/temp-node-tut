9747a89f1ec8b55d467c0818c0b8ec6af0a81799
Waseem Francis <waseem@Waseems-MacBook-Pro.local>
2022-07-02 22:49:25 +0300

    first commit

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+//local
+const secret = `super secret`;
+//share
+
+const john = `hello`;
+const waseem = `waseem`;
+
+
+module.exports =  { john , waseem }
---
+++ modules-callback.js
@@ -0,0 +1,18 @@
+const {readFile,writeFile} = require('fs');
+
+
+readFile('./content/first.txt','utf8',(err,result)=>{
+    if(err){
+        console.lot('error');
+        return;
+    }
+    const first = result;
+    readFile('./content/second.txt','utf8',(err,result)=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+        const second = result;
+        console.log(second);
+    })
+})
